e9bc865e7cad92e7f3c94ae3b4463876561bd4f0
bmoya <bmoya17@hotmail.com>
2021-08-26 09:59:37 -0500

    Feat: :bulb: creación de tabla, entidades y datos de pruebas para el blog
    Signed-off-by: bmoya <bmoya17@hotmail.com>

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,23 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use Sluggable;
+
+    /**
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        App\User::create([
+            'name' => 'Brayan Moya',
+            'email' => 'bmoya17hotmail.com',
+            'password' => bcrypt('betamaster1')
+        ]);
+
+        factory(App\Post::class, 24)->create();
     }
 }
